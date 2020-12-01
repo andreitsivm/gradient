09fa07e673e10342312c41dad7b9906d36d19759
Andreitsiv <andreitsivwork@gmail.com>
2020-12-01 11:41:17 +0200

    init

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,5 @@
 import React from "react";
 import classes from "./Container.module.css";
-import propTypes from "prop-types";
 
 const Container = ({ children }) => {
   return <div className={classes.container}>{children}</div>;
